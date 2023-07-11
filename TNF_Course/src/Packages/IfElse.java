45d67fdf9478c185c3fbacc5406b10e13105b8df
sandeep <8453368444sandy@gmail.com>
2023-07-11 12:08:29 +0530

    second commit IF ELSE

---
+++ TNF_Course/src/Packages/IfElse.java
@@ -0,0 +1,12 @@
+package Packages;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Right to Vote :");
+        } else {
+            System.out.println("Not Right to Vote :");
+        }
+    }
+}
